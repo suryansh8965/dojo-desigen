48615601b8b9e380e97c7a59b4e0d7051a536ca5
suryansh raghuwanshi <raghuwanshisuryansh999@gmail.com>
2022-06-26 19:29:35 +0530

    initial commit

---
+++ music.js
@@ -0,0 +1,4 @@
+var play=document.querySelector(".back-play").addEventListener("click",function(){
+    document.querySelector(".music-text").style.AudioContext="land"
+})
+var text =document.querySelector(".music-text")
